22df39b97f9132c86f11f6219a9f2473cac2858d
iolugovoy <1488221292>
2021-02-26 17:49:50 +0300

    file url fix

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<!DOCTYPE html><html lang=""><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/favicon.ico"><title>vue</title><link href="/css/app.23703a66.css" rel="preload" as="style"><link href="/js/app.f91e801e.js" rel="preload" as="script"><link href="/js/chunk-vendors.46e6aa52.js" rel="preload" as="script"><link href="/css/app.23703a66.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but vue doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app" data-base="/api.php"></div><script src="/js/chunk-vendors.46e6aa52.js"></script><script src="/js/app.f91e801e.js"></script></body></html>
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="/favicon.ico">
+    <title>vue</title>
+    <link href="/css/app.80271617.css" rel="preload" as="style">
+    <link href="/js/app.09fb0aaf.js" rel="preload" as="script">
+    <link href="/js/chunk-vendors.46e6aa52.js" rel="preload" as="script">
+    <link href="/css/app.80271617.css" rel="stylesheet">
+</head>
+
+<body><noscript><strong>We're sorry but vue doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
+<div id="app" data-base="/api.php"></div>
+<script src="/js/chunk-vendors.46e6aa52.js"></script>
+<script src="/js/app.09fb0aaf.js"></script>
+</body>
+
+</html>
